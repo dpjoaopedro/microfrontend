618b5af4081ba3890585115be96700796560f335
Joao Pedro <dp.joaopedro@gmail.com>
2021-03-10 13:17:43 -0300

    Initial commit

---
+++ container/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import MarketingApp from "../components/MarketingApp";
+
+export default () => {
+  return (
+    <Fragment>
+      <h1>Hi There</h1>
+      <hr />
+      <MarketingApp />
+    </Fragment>
+  );
+};
