b736e56c8338f3a88e418d0fea43276bc481e29d
Joao Pedro <dp.joaopedro@gmail.com>
2021-03-11 13:47:19 -0300

    Fix css colision

--- container/src/App.js
+++ container/src/App.js
@@ -2,14 +2,24 @@ import React, { Fragment } from 'react';
 import Header from '../components/Header';
 import MarketingApp from '../components/MarketingApp';
 import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
   return (
-    <BrowserRouter>
-      <Fragment>
-        <Header />
-        <MarketingApp />
-      </Fragment>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <Fragment>
+          <Header />
+          <MarketingApp />
+        </Fragment>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
